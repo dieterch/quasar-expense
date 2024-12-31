8c8261c1b81a1fd8856e27b4cb61b9f1b7be2602
dieterch <dieter.chvatal@gmail.com>
2024-12-31 10:35:11 +0100

    lighter background

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <q-layout view="hHh Lpr fFf">
     <q-img
-      src="background5.png"
+      src="background6.png"
       class="fit absolute"
     />
     <q-header elevated class="bg-primary" >
